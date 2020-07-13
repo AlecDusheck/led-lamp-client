8c9e0a156e4de1cb064bab66f7ed1f30dc5f8062
alecdusheck <alec@simplyalec.com>
2020-07-12 23:21:04 -0500

    Initial Commit

---
+++ colors.ts
@@ -0,0 +1,19 @@
+import {LedColor} from "ws2801-pi/dist";
+
+export const ORANGE: LedColor = {
+    red: 255,
+    green: 165,
+    blue: 0,
+}
+
+export const RED: LedColor = {
+    red: 255,
+    green: 0,
+    blue: 0,
+}
+
+export const WHITE: LedColor = {
+    red: 255,
+    green: 255,
+    blue: 255,
+}
---
+++ main.ts
@@ -0,0 +1,63 @@
+import LedController, {LedColor} from "ws2801-pi/dist";
+import {ORANGE, RED, WHITE} from "./colors";
+
+const io = require('socket.io-client')
+
+enum LedAnimation {
+    RED_GLOW = 'RED_GLOW',
+    RAINBOW_GLOW = 'RAINBOW_GLOW',
+    WHITE = 'WHITE',
+    RED = 'RED',
+    ORANGE = 'ORANGE'
+}
+
+const getLedStatusByString = (animation: string): LedAnimation => {
+    let map = {
+        'RED_GLOW': LedAnimation.RED_GLOW,
+        'RAINBOW_GLOW': LedAnimation.RAINBOW_GLOW,
+        'WHITE': LedAnimation.WHITE,
+        'RED': LedAnimation.RED,
+        'ORANGE': LedAnimation.ORANGE,
+    }
+
+    return map[animation] || LedAnimation.WHITE;
+}
+
+const staticColorAnimation = async (color: LedColor, controller: LedController) => {
+    await controller.fillLeds(color).show();
+    await new Promise(resolve => setTimeout(() => resolve(), 60 * 1000));
+}
+
+(async () => {
+    const LED_COUNT = 60;
+    const ledController = new LedController(LED_COUNT);
+
+    let currentAnimation: LedAnimation;
+
+    console.log('Clearing LEDs...');
+    await ledController.clearLeds().show(); // Clear LEDs on controller
+
+    console.log('Connecting via socket');
+    const socket = io('https://lamp.simplyalec.com');
+
+    socket.on('connect', () => console.log('Socket connected!'));
+    socket.on('disconnect', () => console.log('Socket disconnect'));
+    socket.on('status', data => currentAnimation = getLedStatusByString(data));
+
+    while (true) {
+        switch (currentAnimation) {
+            case LedAnimation.WHITE:
+                await staticColorAnimation(WHITE, ledController);
+                break;
+            case LedAnimation.ORANGE:
+                await staticColorAnimation(ORANGE, ledController);
+                break;
+            case LedAnimation.RED:
+                await staticColorAnimation(RED, ledController);
+                break;
+            default:
+                await staticColorAnimation(WHITE, ledController);
+                break;
+        }
+    }
+})();
